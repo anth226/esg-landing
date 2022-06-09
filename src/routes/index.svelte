4822d40dcb5afeee14e15a2ca123f11237646f10
Ambar Mutha <ambar.ytl@gmail.com>
2022-06-09 18:15:20 +0530

    fix: overflow on mobile screens

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -8,7 +8,7 @@
 </svelte:head>
 
 <div class="bg-base-100 flex-1 flex flex-col">
-	<div class="max-w-5xl mx-auto px-2 flex-1 flex flex-col justify-center">
+	<div class="max-w-5xl w-full mx-auto px-2 flex-1 flex flex-col justify-center">
 		<div class="flex flex-col lg:flex-row w-full justify-between items-center py-4">
 			<div class="max-w-sm">
 				<h1 class="text-5xl">
