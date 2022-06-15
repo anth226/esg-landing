6c4785dbc82340185d5cd0d289bac57d4a8fb386
endlessorhub <anthonyhutchi99@gmail.com>
2022-06-15 11:09:18 +0530

    replace tsparticles with bg image

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,62 +1,14 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
-	import { loadStarsPreset } from 'tsparticles-preset-stars';
-	import type { Engine } from 'tsparticles-engine';
-	import type { SvelteComponent } from 'svelte/internal';
-
 	import Globe from '$lib/components/Globe.svelte';
 	import Stat from '$lib/components/Stat.svelte';
-
-	let ParticlesComponent: SvelteComponent;
-
-	onMount(async () => {
-		const module = await import('svelte-particles');
-		ParticlesComponent = module.default as any;
-	});
-
-	const particlesInit = async (main: Engine) => {
-		await loadStarsPreset(main);
-	};
-
-	const particlesOptions = {
-		background: {
-			color: 'transparent',
-		},
-		particles: {
-			move: {
-				direction: 'none',
-				enable: true,
-				outModes: {
-					default: 'out',
-				},
-				random: true,
-				speed: 0.1,
-				straight: false,
-			},
-			opacity: {
-				animation: {
-					enable: true,
-					speed: 0.3,
-					sync: false,
-				},
-				value: { min: 0, max: 0.25 },
-			},
-			size: {
-				value: { min: 1, max: 3 },
-			},
-		},
-		style: {
-			position: 'static',
-		},
-	};
 </script>
 
 <svelte:head>
 	<title>ESG Impact</title>
 </svelte:head>
 
-<section class="flex-1 flex flex-col relative gradient-background">
-	<div class="max-w-6xl w-full mx-auto px-2 flex-1 flex flex-col justify-center relative z-10">
+<section class="flex-1 flex flex-col gradient-background">
+	<div class="max-w-6xl w-full mx-auto px-2 flex-1 flex flex-col justify-center">
 		<div class="flex flex-col lg:flex-row w-full justify-between items-center py-4">
 			<div class="max-w-lg">
 				<h1 class="text-6xl">
@@ -91,12 +43,6 @@
 			<Stat title="Stakeholder Reports" value="10,000 +" />
 		</div>
 	</div>
-	<svelte:component
-		this={ParticlesComponent}
-		id="particles"
-		options={particlesOptions}
-		{particlesInit}
-	/>
 	<svg
 		xmlns="http://www.w3.org/2000/svg"
 		fill="none"
@@ -122,15 +68,8 @@
 </section>
 
 <style lang="postcss">
-	:global(#particles) {
-		@apply absolute top-0 left-0 w-full h-full;
-	}
-
 	.gradient-background {
-		background: linear-gradient(
-			225deg,
-			rgba(203, 255, 252, 0.5578606442577031) 0%,
-			rgba(17, 51, 69, 1) 50%
-		);
+		background: url('/assets/particles.png'),
+			linear-gradient(225deg, rgba(203, 255, 252, 0.5578606442577031) 0%, rgba(17, 51, 69, 1) 50%);
 	}
 </style>
