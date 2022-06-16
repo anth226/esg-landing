7f778488d081dfc207b54bef6594ed29ac3fa152
anth226 <anthonyhutchi99@gmail.com>
2022-06-16 07:19:19 -0400

    added todos

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,30 +1,8 @@
-
-<section class="flex-1 flex flex-col relative about-header">
-	<div class="max-w-6xl w-full mx-auto px-2 flex-1 flex flex-col mt-24">
-		<div class='text-base-100'>
-	<p>The mission of ESG Impact is to democratize ESG for private markets.</p>
-	<p>
-		We provide accessible and pragmatic frameworks for companies and funds to assess and address ESG
-		metrics most material to their operations. We believe that business ethics should not be
-		transactional or cost a fortune. We understand that 'greenwashing' is the result of pushing ESG
-		too far, using the complexity and myriad of frameworks as a means for confusion instead of a
-		call for unified core metrics.
-	</p>
-
-	<p>
-		Our core values are: <span class='text-primary'>Quality</span>, doing things extraordinarily well or not at all; <span class='text-primary'>Inspiration</span>,
-		uplifting ourselves and our peers; <span class='text-primary'>Diversity</span>, reaping the benefits of having different
-		perspectives; and <span class='text-primary'>Transparency</span>, being authentic in action and displaying a boldness to unveil
-		the truth. We have decided on these not for their encompassment of ethics, but for the
-		applicability to our business operations and dynamisms of our founding team.
-	</p>
-</div>
-	</div>
-</section>
-
+<!-- <img src="/examples/team-example.webp" alt="ex" /> -->
+<div>about</div>
 <style>
-    .about-header{
-        background: white;
-        height: 750px;
-    }
+	.about-header {
+		background: white;
+		height: 750px;
+	}
 </style>
