65469e91359bfab54928c18ac650dd0898155e57
Ambar Mutha <ambar.ytl@gmail.com>
2022-06-19 21:59:50 +0530

    fix: change page transition to fade

--- src/lib/components/PageTransition.svelte
+++ src/lib/components/PageTransition.svelte
@@ -1,10 +1,10 @@
 <script>
-	import { fly } from 'svelte/transition';
+	import { fade } from 'svelte/transition';
 	export let key = '';
 </script>
 
 {#key key}
-	<main in:fly={{ x: -50, duration: 500, delay: 500 }} out:fly={{ x: 50, duration: 500 }}>
+	<main in:fade={{ duration: 500, delay: 500 }} out:fade={{ duration: 500 }}>
 		<slot />
 	</main>
 {/key}
