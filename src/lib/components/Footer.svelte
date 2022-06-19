3ed6d39473dfc79e8a4702e75dc8f56e9436c8f3
Ambar Mutha <ambar.ytl@gmail.com>
2022-06-19 23:19:07 +0530

    make footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,12 +1,60 @@
-<section class="footer max-w-6xl mx-auto mt-16">
-	<div class="text-white text-lg">Software</div>
-	<div class="text-white text-lg">Solutions</div>
-	<div class="text-white text-lg">Pricing</div>
-	<div class="text-white text-lg">About Us</div>
-</section>
+<footer class="flex flex-wrap items-stretch px-16">
+	<div class="flex-1">
+		<div class="flex flex-wrap gap-16 py-24">
+			<h2 class="text-3xl text-white">ESG Impact</h2>
+			<div class="text-lg">
+				Create a better workplace
+				<br />
+				for everyone
+			</div>
+		</div>
 
-<style>
-	.footer {
-		height: 250px;
-	}
-</style>
+		<div class="flex flex-wrap gap-20 py-16">
+			<div>
+				<h3 class="text-xl font-bold mb-2 text-white">Resources</h3>
+				<ul>
+					<li><a href="/">Homepage</a></li>
+					<li><a href="/solutions">Solutions</a></li>
+					<li><a href="/pricing">Pricing</a></li>
+					<li><a href="/about">About Us</a></li>
+				</ul>
+			</div>
+			<div>
+				<h3 class="text-xl font-bold mb-2 text-white">Resources</h3>
+				<ul>
+					<li><a href="/">Homepage</a></li>
+					<li><a href="/solutions">Solutions</a></li>
+					<li><a href="/pricing">Pricing</a></li>
+					<li><a href="/about">About Us</a></li>
+				</ul>
+			</div>
+			<div>
+				<h3 class="text-xl font-bold mb-2 text-white">Resources</h3>
+				<ul>
+					<li><a href="/">Homepage</a></li>
+					<li><a href="/solutions">Solutions</a></li>
+					<li><a href="/pricing">Pricing</a></li>
+					<li><a href="/about">About Us</a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+	<div class="flex-1 px-20 py-24">
+		<div class="uppercase font-sans text-sm mb-4">See ESG Impact in action</div>
+		<h2 class="text-5xl text-white mb-4">
+			Request a<br />demo
+		</h2>
+		<button class="btn text-lg gap-2 btn-primary text-white font-bold">
+			Get Started
+			<svg
+				class="h-4 w-4"
+				viewBox="0 0 16 11"
+				fill="none"
+				stroke="currentColor"
+				xmlns="http://www.w3.org/2000/svg"
+			>
+				<path d="M0 5.318h13.808m0 0L10 1.5m3.808 3.818L10 9.5" />
+			</svg>
+		</button>
+	</div>
+</footer>
