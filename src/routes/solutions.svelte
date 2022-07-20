404eb32790764422285b35b9218dabb97023e5c3
Raqueebuddin Aziz <raqueebuddin.aziz.314@gmail.com>
2022-07-20 11:08:19 +0530

    add solutions page

--- src/routes/solutions.svelte
+++ src/routes/solutions.svelte
@@ -1,16 +1,143 @@
-<div class="bg-white text-black">A Platform Built for Everyone.</div>
-<div class="bg-white text-primary">
-	Top section is dropdown of all customer solution groups. Section underneath is 'METHODOLOGIES /
-	FEATURES'.
+<div class="grid grid-cols-2 place-items-center">
+	<div class="p-10 font-inter">
+		<h2 class="text-2xl font-bold mb-5 text-[#5D6175]">Solving All ESG Pain Points</h2>
+		<p class="leading-relaxed">
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+			labore et dolore magna aliqua. Turpis in eu mi bibendum neque egestas. Risus in hendrerit
+			gravida rutrum quisque non. Quam id leo in vitae turpis massa sed. Vulputate ut pharetra sit
+			amet aliquam id diam maecenas. Urna nunc id cursus metus aliquam. Augue eget arcu dictum
+			varius duis at consectetur lorem donec. Leo duis ut diam quam nulla porttitor massa id.
+			Commodo elit at imperdiet dui accumsan sit amet nulla. Augue interdum velit euismod in
+			pellentesque massa. Morbi enim nunc faucibus a pellentesque. Arcu felis bibendum ut tristique
+			et egestas. Ornare suspendisse sed nisi lacus. Orci sagittis eu volutpat odio facilisis mauris
+			sit. Dictum non consectetur a erat nam at lectus.
+		</p>
+	</div>
+	<img src="/assets/ecology1.png" />
+	<img src="/assets/ecology2.png" />
+	<div class="p-10 font-inter">
+		<h2 class="text-2xl font-bold mb-5 text-[#5D6175]">Solving All ESG Pain Points</h2>
+		<p class="leading-relaxed">
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+			labore et dolore magna aliqua. Turpis in eu mi bibendum neque egestas. Risus in hendrerit
+			gravida rutrum quisque non. Quam id leo in vitae turpis massa sed. Vulputate ut pharetra sit
+			amet aliquam id diam maecenas. Urna nunc id cursus metus aliquam. Augue eget arcu dictum
+			varius duis at consectetur lorem donec. Leo duis ut diam quam nulla porttitor massa id.
+			Commodo elit at imperdiet dui accumsan sit amet nulla. Augue interdum velit euismod in
+			pellentesque massa. Morbi enim nunc faucibus a pellentesque. Arcu felis bibendum ut tristique
+			et egestas. Ornare suspendisse sed nisi lacus. Orci sagittis eu volutpat odio facilisis mauris
+			sit. Dictum non consectetur a erat nam at lectus.
+		</p>
+	</div>
 </div>
-<div class="solutions-header text-black">
-	<div>EITHER one page, 4 #'s on page OR 4 subpages clickable at top</div>
-	<div>Private Companies, Public Companies, Private Equity, Venture Capital</div>
+<div class="grid grid-cols-[auto,1fr] font-inter gap-10 p-10">
+	<aside>
+		<ul>
+			<li class="cursor-pointer p-1 font-bold text-[#ADAEB7] text-lg hover:border-b-2">
+				Venture Capital Funds
+			</li>
+			<li class="cursor-pointer p-1 font-bold text-[#ADAEB7] text-lg hover:border-b-2">
+				Private Equity Groups
+			</li>
+			<li class="cursor-pointer p-1 font-bold text-[#ADAEB7] text-lg hover:border-b-2">
+				Private Companies
+			</li>
+		</ul>
+	</aside>
+	<main class="grid gap-5">
+		<header class="grid grid-cols-[1fr,auto] gap-10 items-center">
+			<h2 class="text-3xl uppercase font-bold text-[#5D6175]">
+				How does ESG impact meet an <span class="text-[#9ccbbf]">asset manager's</span> needs?
+			</h2>
+			<button class="rounded-full text-white text-sm font-bold p-4 bg-[#9ccbbf]"
+				>Schedule a demo now</button
+			>
+		</header>
+		<table class="col-span-full">
+			<div class="relative  grid grid-cols-2">
+				<th class="text-xl font-normal"> Asset manager's need... </th>
+				<th class="text-xl font-normal"> ESG Impact has... </th>
+			</div>
+			<div
+				class="relative  grid grid-cols-2 align-center border-2 align-center my-5 min-h-[7.5rem]"
+			>
+				<td>ESG-related asset allocation customization capabilities</td>
+				<td
+					>Incorporate house views in the most comprehensive & granular sustainability tech kit
+					available with broad, reliable data coverage of 2-3x more than alternatives (30,000+
+					companies, 220,000+ funds, 198 countries, and 188 local governments)</td
+				>
+				<div
+					class="font-bold text-[#9ccbbf] aspect-square w-20 h-20 opacity-75 absolute left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2 bg-white rounded-full grid place-items-center text-2xl p-5 border"
+				>
+					1
+				</div>
+			</div>
+			<div
+				class="relative  grid grid-cols-2 align-center border-2 align-center my-5 min-h-[7.5rem]"
+			>
+				<td>Portfolio reporting for regulators &amp; clients</td>
+				<td
+					>3 clicks from portfolio to report with comprehensive, always-up-to-date SFDR, EU
+					Taxonomy, and TCFD capabilities</td
+				>
+				<div
+					class="font-bold text-[#9ccbbf] aspect-square w-20 h-20 opacity-75 absolute left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2 bg-white rounded-full grid place-items-center text-2xl p-5 border"
+				>
+					2
+				</div>
+			</div>
+			<div
+				class="relative  grid grid-cols-2 align-center border-2 align-center my-5 min-h-[7.5rem]"
+			>
+				<td>Advanced reporting vs UN Sustainable Development Goals</td>
+				<td
+					>Go beyond overall revenue alignment to the impact of a company's operations and it's
+					different products and services for all 16 measurable UN SDGs</td
+				>
+				<div
+					class="font-bold text-[#9ccbbf] aspect-square w-20 h-20 opacity-75 absolute left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2 bg-white rounded-full grid place-items-center text-2xl p-5 border"
+				>
+					3
+				</div>
+			</div>
+			<div
+				class="relative  grid grid-cols-2 align-center border-2 align-center my-5 min-h-[7.5rem]"
+			>
+				<td>Seamless integration into your existing workflow</td>
+				<td>Fully-adaptable API connection or web-based widgets</td>
+				<div
+					class="font-bold text-[#9ccbbf] aspect-square w-20 h-20 opacity-75 absolute left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2 bg-white rounded-full grid place-items-center text-2xl p-5 border"
+				>
+					4
+				</div>
+			</div>
+		</table>
+	</main>
 </div>
 
-<style>
-	.solutions-header {
-		background: white;
-		height: 500px;
+<style lang="postcss">
+	* {
+		box-sizing: border-box;
+	}
+	.font-inter {
+		font-family: Inter, sans-serif;
+	}
+	td,
+	th {
+		@apply p-6;
+	}
+	td {
+		@apply grid items-center;
+	}
+	td:nth-child(2n-1),
+	th:nth-child(2n-1) {
+		@apply bg-gray-100;
+	}
+	td:nth-child(2n -1) {
+		@apply pr-10;
+	}
+	td:nth-child(2n) {
+		@apply text-right pl-10;
 	}
 </style>
