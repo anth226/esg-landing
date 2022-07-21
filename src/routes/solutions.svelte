8fcf66170ab508a1e269b394241a6f42b83c9f80
Raqueebuddin Aziz <raqueebuddin.aziz.314@gmail.com>
2022-07-21 23:39:08 +0530

    solutions tabbing table

--- src/routes/solutions.svelte
+++ src/routes/solutions.svelte
@@ -1,3 +1,11 @@
+<script lang="ts">
+	let activeTab: number = 0;
+
+	function setActiveTab(index: number) {
+		activeTab = index;
+	}
+</script>
+
 <section class="bg-[#F7F7F7] shadow">
 	<div class="max-w-6xl mx-auto ">
 		<div class="flex flex-col w-full justify-center items-start py-4">
@@ -22,30 +30,35 @@
 			>
 				<li
 					class=" cursor-pointer p-1 font-bold text-[#757575] text-lg border-b-2 hover:border-primary"
+					on:click={() => setActiveTab(0)}
 				>
 					<i class="absolute fa-duotone fa-briefcase " />
 					<span class="ml-14">Venture Capital</span>
 				</li>
 				<li
 					class="cursor-pointer p-1 font-bold text-[#757575] text-lg border-b-2 hover:border-primary"
+					on:click={() => setActiveTab(1)}
 				>
 					<i class="absolute fa-duotone fa-circle-nodes " />
 					<span class="ml-14">Private Equity</span>
 				</li>
 				<li
 					class="cursor-pointer p-1 font-bold text-[#757575] text-lg border-b-2 hover:border-primary"
+					on:click={() => setActiveTab(2)}
 				>
 					<i class="absolute fa-duotone fa-house-building " />
 					<span class="ml-14">Public Companies</span>
 				</li>
 				<li
 					class="cursor-pointer p-1 font-bold text-[#757575] text-lg border-b-2 hover:border-primary"
+					on:click={() => setActiveTab(3)}
 				>
 					<i class="absolute fa-duotone fa-briefcase " />
 					<span class="ml-14">Private Companies</span>
 				</li>
 				<li
 					class="cursor-pointer p-1 font-bold text-[#757575] text-lg border-b-2 hover:border-primary"
+					on:click={() => setActiveTab(4)}
 				>
 					<i class="absolute fa-duotone fa-book-open-reader " />
 					<span class="ml-14">Start-ups</span>
@@ -53,75 +66,381 @@
 			</ul>
 		</aside>
 		<main class="grid gap-5">
-			<header class="grid grid-cols-[1fr,auto] gap-10 items-center">
-				<h2 class="text-xl font-bold text-[#757575]">
-					How ESG Impact solves <span class="text-primary">{'Venture Capital'}'s</span> pain points...
-				</h2>
-				<div class="text-white btn btn-sm glass font-bold bg-primary hover:bg-secondary">
-					Schedule a Demo
-				</div>
-			</header>
-			<hr />
-			<table class="col-span-full">
-				<div class="relative grid grid-cols-2 border-2">
-					<th class="text-xl font-normal">Venture Capital need... </th>
-					<th class="text-xl font-normal">ESG Impact has... </th>
-				</div>
-				<div
-					class="relative grid grid-cols-2 align-center border-2 align-center my-5 min-h-[7.5rem]"
-				>
-					<td>ESG-related asset allocation customization capabilities</td>
-					<td
-						>Incorporate house views in the most comprehensive & granular sustainability tech kit
-						available with broad, reliable data coverage of 2-3x more than alternatives (30,000+
-						companies, 220,000+ funds, 198 countries, and 188 local governments)</td
-					>
-					<div
-						class="font-bold text-primary aspect-square w-20 h-20 opacity-75 absolute left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2 bg-white rounded-full grid place-items-center text-2xl p-5 border"
-					>
-						1
+			{#if activeTab === 0}
+				<header class="grid grid-cols-[1fr,auto] gap-10 items-center">
+					<h2 class="text-xl font-bold text-[#757575]">
+						How ESG Impact solves <span class="text-primary">{'Venture Capital'}'s</span> pain points...
+					</h2>
+					<div class="text-white btn btn-sm glass font-bold bg-primary hover:bg-secondary">
+						Schedule a Demo
 					</div>
-				</div>
-				<div
-					class="relative  grid grid-cols-2 align-center border-2 align-center my-5 min-h-[7.5rem]"
-				>
-					<td>Portfolio reporting for regulators &amp; clients</td>
-					<td
-						>3 clicks from portfolio to report with comprehensive, always-up-to-date SFDR, EU
-						Taxonomy, and TCFD capabilities</td
-					>
-					<div
-						class="font-bold text-primary aspect-square w-20 h-20 opacity-75 absolute left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2 bg-white rounded-full grid place-items-center text-2xl p-5 border"
-					>
-						2
+				</header>
+				<hr />
+				<table class="col-span-full">
+					<div class="relative grid grid-cols-2 border-2">
+						<th class="text-xl font-normal">Venture Capital need... </th>
+						<th class="text-xl font-normal">ESG Impact has... </th>
 					</div>
-				</div>
-				<div
-					class="relative  grid grid-cols-2 align-center border-2 align-center my-5 min-h-[7.5rem]"
-				>
-					<td>Advanced reporting vs UN Sustainable Development Goals</td>
-					<td
-						>Go beyond overall revenue alignment to the impact of a company's operations and it's
-						different products and services for all 16 measurable UN SDGs</td
-					>
 					<div
-						class="font-bold text-primary aspect-square w-20 h-20 opacity-75 absolute left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2 bg-white rounded-full grid place-items-center text-2xl p-5 border"
+						class="relative grid grid-cols-2 align-center border-2 align-center my-5 min-h-[7.5rem]"
 					>
-						3
+						<td>ESG-related asset allocation customization capabilities</td>
+						<td
+							>Incorporate house views in the most comprehensive & granular sustainability tech kit
+							available with broad, reliable data coverage of 2-3x more than alternatives (30,000+
+							companies, 220,000+ funds, 198 countries, and 188 local governments)</td
+						>
+						<div
+							class="font-bold text-primary aspect-square w-20 h-20 opacity-75 absolute left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2 bg-white rounded-full grid place-items-center text-2xl p-5 border"
+						>
+							1
+						</div>
 					</div>
-				</div>
-				<div
-					class="relative  grid grid-cols-2 align-center border-2 align-center my-5 min-h-[7.5rem]"
-				>
-					<td>Seamless integration into your existing workflow</td>
-					<td>Fully-adaptable API connection or web-based widgets</td>
 					<div
-						class="font-bold text-primary aspect-square w-20 h-20 opacity-75 absolute left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2 bg-white rounded-full grid place-items-center text-2xl p-5 border"
+						class="relative  grid grid-cols-2 align-center border-2 align-center my-5 min-h-[7.5rem]"
 					>
-						4
+						<td>Portfolio reporting for regulators &amp; clients</td>
+						<td
+							>3 clicks from portfolio to report with comprehensive, always-up-to-date SFDR, EU
+							Taxonomy, and TCFD capabilities</td
+						>
+						<div
+							class="font-bold text-primary aspect-square w-20 h-20 opacity-75 absolute left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2 bg-white rounded-full grid place-items-center text-2xl p-5 border"
+						>
+							2
+						</div>
 					</div>
-				</div>
-			</table>
+					<div
+						class="relative  grid grid-cols-2 align-center border-2 align-center my-5 min-h-[7.5rem]"
+					>
+						<td>Advanced reporting vs UN Sustainable Development Goals</td>
+						<td
+							>Go beyond overall revenue alignment to the impact of a company's operations and it's
+							different products and services for all 16 measurable UN SDGs</td
+						>
+						<div
+							class="font-bold text-primary aspect-square w-20 h-20 opacity-75 absolute left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2 bg-white rounded-full grid place-items-center text-2xl p-5 border"
+						>
+							3
+						</div>
+					</div>
+					<div
+						class="relative  grid grid-cols-2 align-center border-2 align-center my-5 min-h-[7.5rem]"
+					>
+						<td>Seamless integration into your existing workflow</td>
+						<td>Fully-adaptable API connection or web-based widgets</td>
+						<div
+							class="font-bold text-primary aspect-square w-20 h-20 opacity-75 absolute left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2 bg-white rounded-full grid place-items-center text-2xl p-5 border"
+						>
+							4
+						</div>
+					</div>
+				</table>
+			{:else if activeTab === 1}
+				<header class="grid grid-cols-[1fr,auto] gap-10 items-center">
+					<h2 class="text-xl font-bold text-[#757575]">
+						How ESG Impact solves <span class="text-primary">{'Venture Capital'}'s</span> pain points...
+					</h2>
+					<div class="text-white btn btn-sm glass font-bold bg-primary hover:bg-secondary">
+						Schedule a Demo
+					</div>
+				</header>
+				<hr />
+				<table class="col-span-full">
+					<div class="relative grid grid-cols-2 border-2">
+						<th class="text-xl font-normal">Heading {activeTab}</th>
+						<th class="text-xl font-normal">Heading {activeTab}</th>
+					</div>
+					<div
+						class="relative grid grid-cols-2 align-center border-2 align-center my-5 min-h-[7.5rem]"
+					>
+						<td>
+							Item 1 - {activeTab}
+						</td>
+						<td>
+							Item 1 - {activeTab}
+						</td>
+						<div
+							class="font-bold text-primary aspect-square w-20 h-20 opacity-75 absolute left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2 bg-white rounded-full grid place-items-center text-2xl p-5 border"
+						>
+							1
+						</div>
+					</div>
+					<div
+						class="relative  grid grid-cols-2 align-center border-2 align-center my-5 min-h-[7.5rem]"
+					>
+						<td>
+							Item 2 - {activeTab}
+						</td>
+						<td>
+							Item 2 - {activeTab}
+						</td>
+						<div
+							class="font-bold text-primary aspect-square w-20 h-20 opacity-75 absolute left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2 bg-white rounded-full grid place-items-center text-2xl p-5 border"
+						>
+							2
+						</div>
+					</div>
+					<div
+						class="relative  grid grid-cols-2 align-center border-2 align-center my-5 min-h-[7.5rem]"
+					>
+						<td>
+							Item 3 - {activeTab}
+						</td>
+						<td>
+							Item 3 - {activeTab}
+						</td>
+						<div
+							class="font-bold text-primary aspect-square w-20 h-20 opacity-75 absolute left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2 bg-white rounded-full grid place-items-center text-2xl p-5 border"
+						>
+							3
+						</div>
+					</div>
+					<div
+						class="relative  grid grid-cols-2 align-center border-2 align-center my-5 min-h-[7.5rem]"
+					>
+						<td>
+							Item 3 - {activeTab}
+						</td>
+						<td>
+							Item 3 - {activeTab}
+						</td>
+						<div
+							class="font-bold text-primary aspect-square w-20 h-20 opacity-75 absolute left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2 bg-white rounded-full grid place-items-center text-2xl p-5 border"
+						>
+							4
+						</div>
+					</div>
+				</table>
+			{:else if activeTab === 2}
+				<header class="grid grid-cols-[1fr,auto] gap-10 items-center">
+					<h2 class="text-xl font-bold text-[#757575]">
+						How ESG Impact solves <span class="text-primary">{'Venture Capital'}'s</span> pain points...
+					</h2>
+					<div class="text-white btn btn-sm glass font-bold bg-primary hover:bg-secondary">
+						Schedule a Demo
+					</div>
+				</header>
+				<hr />
+				<table class="col-span-full">
+					<div class="relative grid grid-cols-2 border-2">
+						<th class="text-xl font-normal">Heading {activeTab}</th>
+						<th class="text-xl font-normal">Heading {activeTab}</th>
+					</div>
+					<div
+						class="relative grid grid-cols-2 align-center border-2 align-center my-5 min-h-[7.5rem]"
+					>
+						<td>
+							Item 1 - {activeTab}
+						</td>
+						<td>
+							Item 1 - {activeTab}
+						</td>
+						<div
+							class="font-bold text-primary aspect-square w-20 h-20 opacity-75 absolute left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2 bg-white rounded-full grid place-items-center text-2xl p-5 border"
+						>
+							1
+						</div>
+					</div>
+					<div
+						class="relative  grid grid-cols-2 align-center border-2 align-center my-5 min-h-[7.5rem]"
+					>
+						<td>
+							Item 2 - {activeTab}
+						</td>
+						<td>
+							Item 2 - {activeTab}
+						</td>
+						<div
+							class="font-bold text-primary aspect-square w-20 h-20 opacity-75 absolute left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2 bg-white rounded-full grid place-items-center text-2xl p-5 border"
+						>
+							2
+						</div>
+					</div>
+					<div
+						class="relative  grid grid-cols-2 align-center border-2 align-center my-5 min-h-[7.5rem]"
+					>
+						<td>
+							Item 3 - {activeTab}
+						</td>
+						<td>
+							Item 3 - {activeTab}
+						</td>
+						<div
+							class="font-bold text-primary aspect-square w-20 h-20 opacity-75 absolute left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2 bg-white rounded-full grid place-items-center text-2xl p-5 border"
+						>
+							3
+						</div>
+					</div>
+					<div
+						class="relative  grid grid-cols-2 align-center border-2 align-center my-5 min-h-[7.5rem]"
+					>
+						<td>
+							Item 3 - {activeTab}
+						</td>
+						<td>
+							Item 3 - {activeTab}
+						</td>
+						<div
+							class="font-bold text-primary aspect-square w-20 h-20 opacity-75 absolute left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2 bg-white rounded-full grid place-items-center text-2xl p-5 border"
+						>
+							4
+						</div>
+					</div>
+				</table>
+			{:else if activeTab === 3}
+				<header class="grid grid-cols-[1fr,auto] gap-10 items-center">
+					<h2 class="text-xl font-bold text-[#757575]">
+						How ESG Impact solves <span class="text-primary">{'Venture Capital'}'s</span> pain points...
+					</h2>
+					<div class="text-white btn btn-sm glass font-bold bg-primary hover:bg-secondary">
+						Schedule a Demo
+					</div>
+				</header>
+				<hr />
+				<table class="col-span-full">
+					<div class="relative grid grid-cols-2 border-2">
+						<th class="text-xl font-normal">Heading {activeTab}</th>
+						<th class="text-xl font-normal">Heading {activeTab}</th>
+					</div>
+					<div
+						class="relative grid grid-cols-2 align-center border-2 align-center my-5 min-h-[7.5rem]"
+					>
+						<td>
+							Item 1 - {activeTab}
+						</td>
+						<td>
+							Item 1 - {activeTab}
+						</td>
+						<div
+							class="font-bold text-primary aspect-square w-20 h-20 opacity-75 absolute left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2 bg-white rounded-full grid place-items-center text-2xl p-5 border"
+						>
+							1
+						</div>
+					</div>
+					<div
+						class="relative  grid grid-cols-2 align-center border-2 align-center my-5 min-h-[7.5rem]"
+					>
+						<td>
+							Item 2 - {activeTab}
+						</td>
+						<td>
+							Item 2 - {activeTab}
+						</td>
+						<div
+							class="font-bold text-primary aspect-square w-20 h-20 opacity-75 absolute left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2 bg-white rounded-full grid place-items-center text-2xl p-5 border"
+						>
+							2
+						</div>
+					</div>
+					<div
+						class="relative  grid grid-cols-2 align-center border-2 align-center my-5 min-h-[7.5rem]"
+					>
+						<td>
+							Item 3 - {activeTab}
+						</td>
+						<td>
+							Item 3 - {activeTab}
+						</td>
+						<div
+							class="font-bold text-primary aspect-square w-20 h-20 opacity-75 absolute left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2 bg-white rounded-full grid place-items-center text-2xl p-5 border"
+						>
+							3
+						</div>
+					</div>
+					<div
+						class="relative  grid grid-cols-2 align-center border-2 align-center my-5 min-h-[7.5rem]"
+					>
+						<td>
+							Item 3 - {activeTab}
+						</td>
+						<td>
+							Item 3 - {activeTab}
+						</td>
+						<div
+							class="font-bold text-primary aspect-square w-20 h-20 opacity-75 absolute left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2 bg-white rounded-full grid place-items-center text-2xl p-5 border"
+						>
+							4
+						</div>
+					</div>
+				</table>
+			{:else if activeTab === 4}
+				<header class="grid grid-cols-[1fr,auto] gap-10 items-center">
+					<h2 class="text-xl font-bold text-[#757575]">
+						How ESG Impact solves <span class="text-primary">{'Venture Capital'}'s</span> pain points...
+					</h2>
+					<div class="text-white btn btn-sm glass font-bold bg-primary hover:bg-secondary">
+						Schedule a Demo
+					</div>
+				</header>
+				<hr />
+				<table class="col-span-full">
+					<div class="relative grid grid-cols-2 border-2">
+						<th class="text-xl font-normal">Heading {activeTab}</th>
+						<th class="text-xl font-normal">Heading {activeTab}</th>
+					</div>
+					<div
+						class="relative grid grid-cols-2 align-center border-2 align-center my-5 min-h-[7.5rem]"
+					>
+						<td>
+							Item 1 - {activeTab}
+						</td>
+						<td>
+							Item 1 - {activeTab}
+						</td>
+						<div
+							class="font-bold text-primary aspect-square w-20 h-20 opacity-75 absolute left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2 bg-white rounded-full grid place-items-center text-2xl p-5 border"
+						>
+							1
+						</div>
+					</div>
+					<div
+						class="relative  grid grid-cols-2 align-center border-2 align-center my-5 min-h-[7.5rem]"
+					>
+						<td>
+							Item 2 - {activeTab}
+						</td>
+						<td>
+							Item 2 - {activeTab}
+						</td>
+						<div
+							class="font-bold text-primary aspect-square w-20 h-20 opacity-75 absolute left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2 bg-white rounded-full grid place-items-center text-2xl p-5 border"
+						>
+							2
+						</div>
+					</div>
+					<div
+						class="relative  grid grid-cols-2 align-center border-2 align-center my-5 min-h-[7.5rem]"
+					>
+						<td>
+							Item 3 - {activeTab}
+						</td>
+						<td>
+							Item 3 - {activeTab}
+						</td>
+						<div
+							class="font-bold text-primary aspect-square w-20 h-20 opacity-75 absolute left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2 bg-white rounded-full grid place-items-center text-2xl p-5 border"
+						>
+							3
+						</div>
+					</div>
+					<div
+						class="relative  grid grid-cols-2 align-center border-2 align-center my-5 min-h-[7.5rem]"
+					>
+						<td>
+							Item 3 - {activeTab}
+						</td>
+						<td>
+							Item 3 - {activeTab}
+						</td>
+						<div
+							class="font-bold text-primary aspect-square w-20 h-20 opacity-75 absolute left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2 bg-white rounded-full grid place-items-center text-2xl p-5 border"
+						>
+							4
+						</div>
+					</div>
+				</table>
+			{/if}
 		</main>
 	</div>
 </section>
